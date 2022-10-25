eba543ca4f32da044ea42a298e13b4ffa96ead59
17-WynnOng <Wynnong12@gmail.com>
2022-10-25 12:32:48 +0800

    Game Over Made. Game Finished, Score Counted when Passing Obstacles, When colliding with obstacles gameover, prevent player from going above game scene, if player falls under game scene gameover

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    public float jumpspeed;
+    private Rigidbody rb;
+
+    public Text scoretext;
+    public int score;
+
 
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
     }
@@ -15,6 +24,38 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.velocity = Vector2.up * jumpspeed;
+        }
+
+        if (transform.position.y >= 3.54f)
+        {
+            transform.position = new Vector3(transform.position.x, 3.54f, transform.position.z);
+        }
+
+        if (transform.position.y <= -4.2f)
+        {
+            SceneManager.LoadScene("SampleScene 2");
+        }
+
+        
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("SampleScene 2");
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Score")
+        {
+            score++;
+            scoretext.text = "Score: " + score;
+        }
     }
 }
